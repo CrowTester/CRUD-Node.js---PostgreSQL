7900625966e35ea9fe32101909cfc347a402bc0c
Crowzinho <crowstarstest2@gmail.com>
2026-05-04 15:42:42 -0300

    database

---
+++ SQL/Class work.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE livros (
+-- id SERIAL PRIMARY KEY,
+-- titulo VARCHAR(255) NOT NULL,
+-- autor VARCHAR(255) NOT NULL,
+-- ano_publicacao INTEGER,
+-- disponivel BOOLEAN DEFAULT true
+-- );
+
+-- create role Davi password '1234';
+
+-- create role adms;
+
+-- grant adms to Davi;
+
+-- grant all on all sequences in schema public to adms;
+-- grant all on all tables in schema public to adms;
